64ba7a633a2124a501de8d0ae102024d213486d0
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2024-12-13 18:56:30 +0330

    add: dark theme by default

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import { Route, Routes } from "react-router";
 import "./assets/App.css";
 import { routes } from "./data/routes";
 import BottomNavigation from "./components/layout/BottomNavigation";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
     <div className="overflow-hidden p-0">
-      <main className=" h-[calc(100dvh-64px)] overflow-auto">
-        <div className="container">
-          <Routes>
-            {routes.map(({ element, path }) => {
-              return <Route key={path} path={path} element={element} />;
-            })}
-          </Routes>
-        </div>
-      </main>
-      <BottomNavigation />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <main className=" h-[calc(100dvh-64px)] overflow-auto">
+          <div className="container">
+            <Routes>
+              {routes.map(({ element, path }) => {
+                return <Route key={path} path={path} element={element} />;
+              })}
+            </Routes>
+          </div>
+        </main>
+        <BottomNavigation />
+      </ThemeProvider>
     </div>
   );
 }
