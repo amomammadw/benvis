58c4734d21df06ce9fa0d39aebb7dcca551bf526
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2024-12-13 18:48:20 +0330

    add: shadcn and stuff

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,48 +1,21 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 function Homepage() {
   return (
     <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, molestiae
-      inventore modi, ad perferendis sed iste amet nostrum quo dolorum delectus
-      temporibus non est commodi deleniti! Voluptates tempore quod pariatur
-      labore a placeat, eius, hic odio laborum beatae expedita, vel quidem
-      natus. Omnis qui nesciunt facilis laudantium, aliquam deleniti maiores
-      cumque aspernatur, sint sit ipsa quidem nulla magnam vero, expedita minima
-      aperiam repellat hic illum quis architecto provident sed? In repellendus
-      libero aliquid corporis cumque enim dolor, nostrum, accusamus ad minus
-      repellat, esse laboriosam magnam. Amet illum assumenda eaque debitis!
-      Molestias rerum nisi ipsam eaque ex officia, neque soluta ipsum delectus
-      quod, architecto, cupiditate cumque tenetur ab mollitia quas ipsa corporis
-      iure perspiciatis? Minus amet et necessitatibus exercitationem officiis
-      quis accusantium sunt, commodi magnam debitis libero blanditiis ducimus
-      harum fugiat repellendus molestias temporibus optio quisquam cumque.
-      Excepturi odio neque voluptatem possimus numquam voluptatum iure quis
-      facilis tempora hic corporis temporibus commodi mollitia omnis, incidunt
-      tenetur velit earum reprehenderit repellat sapiente esse et architecto
-      porro? At sunt blanditiis odit culpa ab, sit dolorem laboriosam officia
-      repellendus ipsam. Fugit quos nesciunt pariatur neque consequatur aut quod
-      rem! Dolore deserunt atque sed molestias velit exercitationem corporis
-      temporibus similique iusto sequi repellendus, dolorum quaerat vel qui et
-      tempore odio unde, perferendis facilis delectus. Deleniti enim blanditiis
-      ab. Sed, iste ab? Necessitatibus exercitationem fuga perferendis. Odit
-      nemo, vel ea unde mollitia totam consequuntur officia deleniti quae
-      numquam optio velit libero earum, laborum dolore. Necessitatibus ipsam
-      quia iure saepe eum, facere dolorem enim quis ratione? Officia ducimus
-      illo laudantium voluptatem earum perspiciatis non debitis optio expedita
-      voluptate animi vero et repellat, minus corrupti odio, aspernatur illum,
-      similique culpa? Rerum sunt illo placeat assumenda sint numquam, cum nulla
-      obcaecati, ab ipsum, modi odit expedita possimus officia consectetur.
-      Fuga, optio, a vel obcaecati cupiditate expedita ipsam officia id
-      aspernatur molestias praesentium at sequi nulla perspiciatis soluta porro
-      nam, corrupti facere quas. Ad quos reprehenderit fuga. Obcaecati laborum
-      cumque sint alias asperiores, vel perferendis a, accusamus inventore
-      aliquam odit quia voluptatem omnis. Saepe animi numquam minima corporis ad
-      adipisci at aliquam. Tempora aut deleniti quis eos ipsam corrupti sunt
-      sint, aspernatur dolorum quas perspiciatis amet fugit corporis atque
-      soluta optio nisi? Sequi accusamus repellendus neque autem eaque
-      cupiditate ducimus iure libero magni consectetur explicabo, unde quas
-      reprehenderit debitis adipisci natus quos consequatur dignissimos esse
-      cumque perspiciatis. Dignissimos commodi, placeat quo doloremque
-      accusantium, tempora eum eius dicta assumenda vitae quisquam.
+      <Accordion type="single">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
